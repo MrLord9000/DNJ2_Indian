1b648ce485f6e2a50f1df2446ba710b6fe6b95f1
Andrzej M <andrzej.mi@op.pl>
2019-09-21 12:40:35 +0200

    UI in Progress

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(RectTransform))]
 public class InventoryUI : MonoBehaviour
 {
     List<IInventoryItem> items;
+    RectTransform rt;
 
     // Start is called before the first frame update
     void Start()
     {
-        items = GameManager.GetInstance().GetInventory().items;
+        items = GameManager.Inventory.items;
+        rt = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
@@ -21,17 +24,29 @@ public class InventoryUI : MonoBehaviour
     [ContextMenu("Refresh")]
     public void Refresh()
     {
-        int space = Screen.width / 100;
+        float space =  rt.rect.width / 100;
 
         Vector2 pointer = new Vector2(space, Screen.height - space);
 
         foreach (IInventoryItem item in items)
         {
+            GameObject obj = Instantiate(GameManager.SlotUIPrefab);
+            Image img = obj.GetComponent<Image>();
+
+            obj.transform.SetParent(transform);
+            obj.transform.position = transform.position;
+            /*
             GameObject obj = new GameObject();
             Image img = obj.AddComponent<Image>();
             RectTransform rt = obj.GetComponent<RectTransform>();
 
             img.sprite = item.Image;
+            rt.SetParent(transform);
+
+            rt.localPosition = pointer;
+            rt.sizeDelta = img.sprite.texture.texelSize;
+
+            pointer.x += item.Image.texture.width + space;*/
 
         }
     }
