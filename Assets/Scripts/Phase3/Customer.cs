4121f7af93ab966395b987f8efd89a5a648a1ca4
Jedrzej-Jahonan-Mrozowski-aka-Lil_Slowacki <jedrzej.mrozowski@gmail.com>
2019-09-21 14:52:30 +0200

    tmp

---
+++ Assets/Scripts/Phase3/Customer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+	private enum PotionEffect
+	{
+	    speed,
+	    slow,
+	    large,
+	    smal,
+	    multiplication,
+	    halucination,
+	    invis,
+	    confusion,
+	    twist,
+	    drunk,
+	    blackNWhite,
+	    rain,
+	    strength,
+	    levitation,
+	    poison
+	}
+
+	private string CustomerPotionEffect;
+
+	void setCustomerPotionEffect(){
+		//CustomerPotionEffect=/*Random from PotionEffect*/
+	}
+
+	int sellPotionToCustomer(GameObject potion){
+		if(CustomerPotionEffect==potion.potionEffect){
+			if(/*effect from 2 tier 1 flowers*/){
+				return 1;
+			}
+			else if(/*effect from 2 tier 2 flowers*/){
+				return 3;
+			}
+			else/*effect from 1 tier 1 and 1 tier 2 flower*/{
+				return 2;
+			}
+		}
+		else return 0;
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setCustomerPotionEffect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
