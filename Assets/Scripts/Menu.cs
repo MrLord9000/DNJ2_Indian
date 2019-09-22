528e637851ec11d6bb7d4c4de0f29b1e34a2bc69
Jedrzej-Jahonan-Mrozowski-aka-Lil_Slowacki <jedrzej.mrozowski@gmail.com>
2019-09-22 02:14:39 +0200

    added MainMenu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+	public GameObject curtain;
+
+    public void StartGame(){
+    	StartCoroutine(Fade(3f));
+    	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void QuitGame(){
+    	StartCoroutine(Fade(3f));
+    	Application.Quit();
+    }
+
+    private IEnumerator Fade(float fadeOutTime)
+     {
+         float t = 0.0f;
+         while (t<1.0f)
+         {
+             
+             t = Mathf.Clamp01(t + Time.deltaTime / fadeOutTime);
+             curtain.GetComponent<Image>().color = new Color(curtain.GetComponent<Image>().color.r,curtain.GetComponent<Image>().color.g,curtain.GetComponent<Image>().color.b,t);
+             yield return new WaitForEndOfFrame();
+         }
+     }
+}
