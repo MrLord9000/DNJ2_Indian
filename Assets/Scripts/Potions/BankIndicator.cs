c51dfe4a47bfbb396dbf195a534bd38527ea0f26
Andrzej M <andrzej.mi@op.pl>
2019-09-22 11:52:19 +0200

    GUI impovement

---
+++ Assets/Scripts/Potions/BankIndicator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class BankIndicator : MonoBehaviour
+{
+    TextMeshProUGUI text;
+    Inventory inventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        inventory = FindObjectOfType<Inventory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = inventory.bank.ToString();
+    }
+}
