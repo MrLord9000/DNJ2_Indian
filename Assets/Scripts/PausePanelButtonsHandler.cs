3eb0a1f39809ea5ea5b6b66473278d1423162129
Jedrzej-Jahonan-Mrozowski-aka-Lil_Slowacki <jedrzej.mrozowski@gmail.com>
2019-09-22 03:43:05 +0200

    Pause Panel added

---
+++ Assets/Scripts/PausePanelButtonsHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausePanelButtonsHandler : MonoBehaviour
+{
+    public static void Resume(GameObject pausePanel){
+    	Time.timeScale=1f;
+    	Destroy(pausePanel);
+    }
+
+    public static void MainMenu(GameObject pausePanel){
+    	Time.timeScale=1f;
+    	SceneManager.LoadScene(0);
+    	Destroy(pausePanel);
+    }
+}
